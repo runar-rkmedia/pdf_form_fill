c5499348a0c9b25fced1ded7bcdd5b7267b6ad6f
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-05-17 19:02:15 +0200

    set_fields_from_product

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import sys
 from config import configure_app
 from models import (db, Manufacturor, Product,
-                   ProductSpec, ProductType, lookup_vk)
+                    ProductSpec, ProductType, lookup_vk)
 from vk_objects import nexans
 from flask import (
     Flask,
@@ -19,10 +19,25 @@ configure_app(app)
 db.init_app(app)
 
 
+def set_fields_from_product(dictionary, product, specs=None):
+    """Set multiple fields from a Product-table."""
+    dictionary["Betegnelse"] = product.name
+    # legg til enleder/toleder
+    ledere = product.product_type.ledere
+    if ledere == 2:
+        dictionary['check-toleder'] = True
+    elif ledere == 1:
+        dictionary['check-enleder'] = True
+
+    for s in specs:
+        if s.key == 'Nominell elementmotstand':
+            dictionary['nominell_motstand'] = s.value
+    return dictionary
+
+
 @app.route('/')
 def view_form():
     """View for home."""
-    lookup_vk('Nexans', 17, 500)
     return render_template(
         'form.html',
     )
@@ -31,7 +46,20 @@ def view_form():
 @app.route('/nexans.html', methods=['POST'])
 def fill_document():
     """Fill a document with data from form, and smart usage."""
-    nexans.set_fields_from_dict(request.form)  # TODO: is this safe?
+    vk_manufacturor = request.form['manufacturor']
+    vk_effekt = request.form['effekt']
+    vk_meterEffekt = request.form['meterEffekt']
+    filtered_vks = lookup_vk(vk_manufacturor, vk_meterEffekt, vk_effekt)
+    dictionary = request.form.copy()
+    if len(filtered_vks) > 1:
+        raise ValueError("Got more than one product. Need more filtering.")
+    else:
+        varmekabel = filtered_vks[0]
+        specs = varmekabel.get_specs()
+        dictionary = set_fields_from_product(
+            dictionary, varmekabel, specs)
+
+    nexans.set_fields_from_dict(dictionary)
     nexans.create_filled_pdf('output.pdf')
     return redirect('/')
 
@@ -52,10 +80,10 @@ if __name__ == "__main__":
                                ledere=2,
                                manufacturor=Nexans)
             txlp_tull = ProductType(name='TXLP/TULL/17',
-                               mainSpec='TXLPTULL',
-                               watt_per_meter=16,
-                               ledere=2,
-                               manufacturor=Nexans)
+                                    mainSpec='TXLPTULL',
+                                    watt_per_meter=16,
+                                    ledere=2,
+                                    manufacturor=Nexans)
             db.session.add(txlp)
             db.session.add(txlp_tull)
             import Nexans_TXLP
