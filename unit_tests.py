e59a7e8c9718cae2066173c8352b0999e2a9de31
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-05-25 10:29:23 +0200

    unittest

--- unit_tests.py
+++ unit_tests.py
@@ -1,20 +1,22 @@
 import os
-import app
+from app import app
 import unittest
 import tempfile
 from config import configure_app
 
+
+
 class FlaskrTestCase(unittest.TestCase):
 
-    flaskr.app.config['TESTING'] = True
-    configure_app(flaskr)
+    app.config['TESTING'] = True
+    configure_app(app)
 
     def setUp(self):
-        self.app = flaskr.app.test_client()
+        self.app = app.test_client()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        os.unlink(app.config['DATABASE'])
 
 if __name__ == '__main__':
     unittest.main()
