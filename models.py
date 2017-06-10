afbc2cec9693f7ad05b3398fd481c947cb5a334a
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-06-10 21:33:12 +0200

    model: Company, user, contact, address

--- models.py
+++ models.py
@@ -2,7 +2,14 @@
 
 from flask_sqlalchemy import SQLAlchemy
 import bleach
+import enum
 db = SQLAlchemy()
+
+
+class ContactType(enum.Enum):
+    phone = 1,
+    email = 2,
+    mobile = 3
 
 
 def lookup_vk(manufacturor, watt_per_meter, watt_total):
@@ -17,6 +24,78 @@ def lookup_vk(manufacturor, watt_per_meter, watt_total):
     for p in products:
         print(p.name)
     return products
+
+
+class Address(db.Model):
+    """Address-table for users."""
+    __tablename__ = 'address'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    linje1 = db.Column(db.String(200))
+    linje2 = db.Column(db.String(200))
+    postnumber = db.Column(db.SmallInteger)
+    postal = db.Column(db.String(200))
+
+
+class Contact(db.Model):
+    """Contact-table for users, like phone, email"""
+    __tablename__ = 'contact'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    type = db.Column(db.Enum(ContactType))
+    value = db.Column(db.String(200))
+
+
+class Company(db.Model):
+    """Company-table for users."""
+    __tablename__ = 'company'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(500))
+    orgnumber = db.Column(db.Integer)
+    address_id = db.Column(db.Integer, db.ForeignKey(Address.id))
+    address = db.relationship(
+        Address, primaryjoin='Company.address_id==Address.id')
+
+
+class CompanyContact(db.Model):
+    """Associations-table for company and contacts."""
+    __tablename__ = 'company_contact'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    contact_id = db.Column(db.Integer, db.ForeignKey(Contact.id))
+    company_id = db.Column(db.Integer, db.ForeignKey(Company.id))
+
+
+class User(db.Model):
+    """User-table for users."""
+    __tablename__ = 'vk_user'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(100))
+    title = db.Column(db.String(50))
+    signature = db.Column(db.Binary())
+    company_id = db.Column(db.Integer, db.ForeignKey(Company.id))
+    company = db.relationship(
+        Company, primaryjoin='User.company_id==Company.id')
+
+    def addContact(self, contact_type, contact_value):
+        """Description."""
+        contact = Contact(type=contact_type, value=contact_value)
+        db.session.add(contact)
+        user_contact = UserContact(
+            contact=contact,
+            user=self
+        )
+        db.session.add(user_contact)
+
+
+class UserContact(db.Model):
+    """Associations-table for user and contacts."""
+    __tablename__ = 'user_contact'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    contact_id = db.Column(db.Integer, db.ForeignKey(Contact.id))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    contact = db.relationship(
+        Contact, primaryjoin='UserContact.contact_id==Contact.id')
+    user = db.relationship(
+        User, primaryjoin='UserContact.user_id==User.id')
 
 
 class Manufacturor(db.Model):
@@ -63,10 +142,10 @@ class ProductType(db.Model):
         products_dict = [i.serialize for i in products]
 
         dictionary = {
-        'id': self.id,
-        'name': self.name,
-        'ledere': self.ledere,
-        'products': products_dict
+            'id': self.id,
+            'name': self.name,
+            'ledere': self.ledere,
+            'products': products_dict
         }
         if self.watt_per_meter:
             dictionary['watt_per_meter'] = self.watt_per_meter
