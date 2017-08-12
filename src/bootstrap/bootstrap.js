00d25d19f4c4a582ff7d47aecddc356af7756f3b
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-08-12 18:00:57 +0200

    bootswatch

--- src/bootstrap/bootstrap.js
+++ src/bootstrap/bootstrap.js
@@ -1,13 +1,13 @@
 import 'bootstrap/js/transition';
-import 'bootstrap/js/alert';
+// import 'bootstrap/js/alert';
 import 'bootstrap/js/button';
-import 'bootstrap/js/carousel';
+// import 'bootstrap/js/carousel';
 import 'bootstrap/js/collapse';
-import 'bootstrap/js/dropdown';
-import 'bootstrap/js/modal';
-import 'bootstrap/js/tooltip';
-import 'bootstrap/js/popover';
-import 'bootstrap/js/scrollspy';
+// import 'bootstrap/js/dropdown';
+// import 'bootstrap/js/modal';
+// import 'bootstrap/js/tooltip';
+// import 'bootstrap/js/popover';
+// import 'bootstrap/js/scrollspy';
 import 'bootstrap/js/tab';
-import 'bootstrap/js/affix';
+// import 'bootstrap/js/affix';
 import './bootstrap.less'
