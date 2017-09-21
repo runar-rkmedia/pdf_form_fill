c9bb1459f510cdcfa57d009278b823717db0fb4d
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-09-21 16:16:54 +0200

    minify css in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,10 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const PurifyCSSPlugin = require('purifycss-webpack');
+const glob = require('glob');
+const path = require('path');
 module.exports = merge(common, {
   plugins: [
     new UglifyJSPlugin(),
@@ -9,6 +13,28 @@ module.exports = merge(common, {
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
+    }),
+    new OptimizeCssAssetsPlugin({
+      // assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true
+        }
+      },
+      canPrint: true
     })
+    // new PurifyCSSPlugin({
+    //   // Give paths to parse for rules. These should be absolute!
+    //   paths: glob.sync(
+    //     path.join(__dirname, 'templates/**/*.html'),
+    //     path.join(__dirname, 'static/js/*.js'),
+    //     path.join(__dirname, 'src/**/*.*')
+    //   ),
+    //   purifyOptions: {
+    //     rejected: true,
+    //     whitelist: ['*bootstrap-datetimepicker*', '*fieldset*"', '*panel*']
+    //   }
+    // })
   ]
 });
