e80c9a182c2f078cd7ae26bb0deb5aa3b0060e3e
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-09-27 23:24:05 +0200

    Design

--- src/main.ts
+++ src/main.ts
@@ -16,27 +16,8 @@ WebFont.load({
 });
 // webpack doesn't like to litter the global-namespace, so to force this function to be available there, we need to add the function to global. then typescript compains, so we need to add to it.
 (<any>window).format_date = (dateString: string, type: string) => {
-  var d_names = new Array("Søndag", "Mandag", "Tirsdag",
-    "Onsdag", "Torsdag", "Fredag", "Søndag");
-
-  let m_names = new Array("januar", "februar", "mars",
-    "april", "mai", "juni", "juli", "august", "september",
-    "october", "november", "december");
-  //
-  // let d = new Date(dateString).toISOString()
   let d = new Date(dateString);
-  let curr_day = d.getDay();
-  let curr_date = d.getDate();
-  let curr_month = d.getMonth();
-  let curr_year = d.getFullYear();
-  let curr_hour = d.getHours();
-  let curr_minute = d.getMinutes();
-  if (type === 'short') {
-    return curr_date + '/' + curr_month + "-" + String(curr_year).slice(2) + ' ' +
-      pad(String(curr_hour), 2) + ':' + pad(String(curr_minute), 2);
-  }
-  return curr_date + '. ' + m_names[curr_month] + " " + curr_year + ' ' +
-    pad(String(curr_hour), 2) + ':' + pad(String(curr_minute), 2);
+  return moment(d).calendar()
 };
 
 $(() => {
